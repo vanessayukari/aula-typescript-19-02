d6ab85d5969ceb67cf2605aec0826c7afa5bd25c
vanessayukari <ykuari@hotmail.com>
2025-02-19 20:51:33 -0300

    exercícios completos

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,25 @@
 "use strict";
 let mensagem = "Olá, TypeScript!";
 console.log(mensagem);
+// Exercício 1
+let valores = [10, 20, 30];
+let somaValores = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma dos valores: ", somaValores);
+// Exercício 2
+function mensagemPersonalizada(nome) {
+    return `Olá, ${nome}! Bem-vindo ao TypesCript.`;
+}
+console.log(mensagemPersonalizada("Ana"));
+// Exercício 3
+class Carro {
+    constructor(marca, modelo, ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+    detalhes() {
+        return `${this.marca} ${this.modelo}, Ano: ${this.ano}`;
+    }
+}
+const carro1 = new Carro("Toyota", "Corolla", 2022);
+console.log(carro1.detalhes());
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
 let mensagem: string = "Olá, TypeScript!";
 console.log(mensagem);
+
+// Exercício 1
+let valores: number[] = [10, 20, 30];
+let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma dos valores: ", somaValores);
+
+// Exercício 2
+function mensagemPersonalizada(nome: string): string {
+    return `Olá, ${nome}! Bem-vindo ao TypesCript.`;
+}
+console.log(mensagemPersonalizada("Ana"));
+
+// Exercício 3
+class Carro {
+    constructor(public marca: string, public modelo: string, public ano: number)
+{}
+    detalhes(): string {
+        return `${this.marca} ${this.modelo}, Ano: ${this.ano}`;
+    }
+}
+
+const carro1 = new Carro("Toyota", "Corolla", 2022);
+console.log(carro1.detalhes());
+
+
